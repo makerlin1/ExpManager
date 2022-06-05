03669656ce4e8642a18659b2f0881edcb0ab1cb8
makerlin1 <2476618387@qq.com>
2022-06-06 00:14:27 +0800

    add Metric and Acc

--- ExpM/utils.py
+++ ExpM/utils.py
@@ -55,3 +55,44 @@ def send_email(info="", **profile):
         sender, receivers, message.as_string())
     # 退出
     smtpObj.quit()
+
+
+class AverageMeter(object):
+    """
+    Computes and stores the average and current value
+    Copied from: https://github.com/pytorch/examples/blob/master/imagenet/main.py
+    """
+
+    def __init__(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.reshape(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Experiment Manager
 - [x] 接受输入配置(包括各种超参数)，生成实验id并记录到leaderboard.csv文件
 - [x] 记录实验结果，添加进leaderboard.csv
 - [x] 实验结束后发送邮件通知（需要自行准备163邮箱账户与秘钥）
-- [ ] 预构建一些基本Metric
+- [x] 预构建一些基本Metric
 ## Quickly Start
 ```python
 from ExpM import Trial
